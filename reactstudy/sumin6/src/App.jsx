5308efbc998f5e003f24119fd0e004b81480cd67
pyatdel <ppp2984@gmail.com>
2024-10-18 21:08:01 +0900

    renamed:    "reactstudy/sijax/public/\354\236\220\353\217\231\353\263\200\355\231\230.html" -> "reactstudy/sijax/public/10-11/\354\236\220\353\217\231\353\263\200\355\231\230.html" 	renamed:    reactstudy/sijax/public/JSON.html -> reactstudy/sijax/public/10-14/JSON.html 	renamed:    "reactstudy/sijax/public/JSON\355\231\234\354\232\251.html" -> "reactstudy/sijax/public/10-14/JSON\355\231\234\354\232\251.html" 	renamed:    reactstudy/sijax/public/test.html -> reactstudy/sijax/public/10-14/test.html 	renamed:    reactstudy/sijax/public/varletconst.html -> reactstudy/sijax/public/10-14/varletconst.html 	renamed:    "reactstudy/sijax/public/\353\260\230\353\263\265\353\254\270.html" -> "reactstudy/sijax/public/10-14/\353\260\230\353\263\265\353\254\270.html" 	renamed:    "reactstudy/sijax/public/\353\263\265\354\202\254\354\231\200\354\260\270\354\241\260.html" -> "reactstudy/sijax/public/10-14/\353\263\265\354\202\254\354\231\200\354\260\270\354\241\260.html" 	renamed:    "reactstudy/sijax/public/\355\225\250\354\210\2302.html" -> "reactstudy/sijax/public/10-14/\355\225\250\354\210\2302.html" 	renamed:    "reactstudy/sijax/public/\355\225\250\354\210\230\354\244\221\354\232\224.html" -> "reactstudy/sijax/public/10-14/\355\225\250\354\210\230\354\244\221\354\232\224.html" 	renamed:    "reactstudy/sijax/public/bind\352\263\240\352\270\211\355\231\234\354\232\251.html" -> "reactstudy/sijax/public/10-15/bind\352\263\240\352\270\211\355\231\234\354\232\251.html" 	renamed:    "reactstudy/sijax/public/this\352\260\225\354\240\234\354\240\234\354\226\264.html" -> "reactstudy/sijax/public/10-15/this\352\260\225\354\240\234\354\240\234\354\226\264.html" 	renamed:    "reactstudy/sijax/public/this\354\227\220\352\264\200\355\225\264\354\204\234.html" -> "reactstudy/sijax/public/10-15/this\354\227\220\352\264\200\355\225\264\354\204\234.html" 	renamed:    "reactstudy/sijax/public/\355\225\250\354\210\2303\353\213\244\354\213\234\353\263\264\352\270\260.html" -> "reactstudy/sijax/public/10-15/\355\225\250\354\210\2303\353\213\244\354\213\234\353\263\264\352\270\260.html" 	renamed:    "reactstudy/sijax/public/function\354\235\204class\353\241\234.html" -> "reactstudy/sijax/public/10-17/function\354\235\204class\353\241\234.html" 	renamed:    "reactstudy/sijax/public/\353\270\224\353\241\234\352\267\270\354\206\214\354\212\244.html" -> "reactstudy/sijax/public/10-17/\353\270\224\353\241\234\352\267\270\354\206\214\354\212\244.html" 	new file:   "reactstudy/sijax/public/10-18/\353\260\260\354\227\264\354\247\201\354\240\221\353\247\214\353\223\244\354\226\264\353\263\264\352\270\260\353\257\270\354\231\204.html" 	new file:   "reactstudy/sijax/public/10-18/\355\224\214\353\246\254\354\273\244.html" 	new file:   "reactstudy/sijax/public/10-18/\355\231\224\354\202\264\355\221\234\355\225\250\354\210\230.html" 	modified:   "reactstudy/sijax/public/\353\246\254\353\267\260.txt" 	deleted:    "reactstudy/sijax/public/\353\260\260\354\227\264\354\247\201\354\240\221\353\247\214\353\223\244\354\226\264\353\263\264\352\270\260.html" 	renamed:    "reactstudy/sijax/public/\353\204\210\353\254\264\354\211\254\354\232\264\354\210\231\354\240\234.html" -> "reactstudy/sijax/public/\354\210\231\354\240\234/\353\204\210\353\254\264\354\211\254\354\232\264\354\210\231\354\240\234.html" 	modified:   "reactstudy/sijax/public/\354\210\231\354\240\234/\354\240\225\353\247\220\354\211\254\354\232\264\354\210\231\354\240\234.html" 	modified:   reactstudy/sumin6/src/App.jsx 	new file:   reactstudy/sumin6/src/Sumin.css 	modified:   reactstudy/sumin6/src/Sumin.jsx 	modified:   reactstudy/sumin6/src/main.jsx

--- reactstudy/sumin6/src/App.jsx
+++ reactstudy/sumin6/src/App.jsx
@@ -1,15 +1,49 @@
 import './App.css'
 import Sumin from './Sumin.jsx'
+
 function App() {
 
-  
+  const suminFriends = [
+      {name:"진모",age:24,alias:"주진모"},
+      {name:"승민",age:24,alias:"E7E"},
+      {name:"진찬",age:24,alias:"수민"},
+      {name:"윤호",age:23,alias:"지영"},
+      {name:"광제",age:27,alias:"완희"},
+      {name:"완희",age:27,alias:"미강"},
+  ];
+
+// 전개 연산자 이용 친구 추가
+// {name:"수민",age:24,alias:"자전"}
+
+const suminFriends2 = [{name:"수민",age:24,alias:"자전"},...suminFriends];
+
 
   return (
     <div>
       <h1>뭘봐</h1>
-      <Sumin name={"육수민"} age={20} alias={"노잼"}/>
-      <hr/ >
-      <Sumin name={"승민"} age={24} alias={"원래재미없지"} />
+      {
+        /* if문 사용 불가 그래서 숙제 && */
+        /* suminFriends && suminFriends.map(
+          sf => (<Sumin name={sf.name} age={sf.age} alias={sf.alias} />
+          )
+          
+        ) */
+
+          /*
+          suminFriends && suminFriends.filter(sf => {
+            if(sf.name == "윤호") return false
+            return true;
+          }) .map(
+            sf => (<Sumin name={sf.name} age={sf.age} alias={sf.alias} />)
+            )
+            */
+
+            // 중요!!!
+            suminFriends2 && suminFriends2.filter(sf => sf.name != "윤호"
+            ) .map(
+              sf => (<Sumin name={sf.name} age={sf.age} alias={sf.alias} />)
+              )
+      }
     </div>
   )
 }
--- reactstudy/sumin6/src/Sumin.jsx
+++ reactstudy/sumin6/src/Sumin.jsx
@@ -1,18 +1,29 @@
 import react from 'react'
+import "./Sumin.css"
 
 // 다른 이름으로 하면 문제가 항상 발생하진 않음
-function Sumin(Props) {
-    const myName="육수민만세"
+// 매개변수 Props에 구조 분해 적용
+// {name, age, alias } = Props
+function Sumin({name,age,alias}) {
 
-    console.log("체크:",Props);
+    // console.log("체크:",Props);
+
+    const handleClick = ()=> {
+      alert("나 눌렀어?");
+    }
+
+    /* 아래 2개를 잘 구분해야 진도 나가는데 어려움이 없음! (중요중요)
+      { handleClick }
+      { () => handleClick() }
+    */
 
   return (
-    <react.Fragment>
-      <h1>{myName}</h1>
-      {/* 이렇게 주석을 이용함 */}
-      <h2>{Props.age}</h2>
-      <h3>{Props.alias}</h3>
-      </react.Fragment>
+    <div onClick= { ()=> {return handleClick() } }  style={{border:"20px groove black",color:"blue"}}>
+      <h1 className='sumin1'>{name}</h1>
+      {/* 이렇게 주석을 이용함! */}
+      <h2>{age}</h2>
+      <h3>{alias}</h3>
+    </div>
   )
 }
 
